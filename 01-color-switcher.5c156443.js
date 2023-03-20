a972abaf23221efa1e888f5665763678801551c4
Serhii-Pradid <115069705+Serhii-Pradid@users.noreply.github.com>
2023-03-20 16:29:10 +0000

    Deploying to gh-pages from @ Serhii-Pradid/goit-js-hw-09@7852999b64e2ba3c0eb111149b7790428ee78585 🚀

---
+++ 01-color-switcher.5c156443.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),o=document.querySelector("body");function r(){o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(function(){colorId=setInterval(r,1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearTimeout(colorId),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.5c156443.js.map
