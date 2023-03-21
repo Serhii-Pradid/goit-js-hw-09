29722a93d81135778e2a380c0c961273008eeda0
Serhii Pradid <115069705+Serhii-Pradid@users.noreply.github.com>
2023-03-21 18:29:57 +0200

    ДЗ 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formSubmit = document.querySelector('.form');
+formSubmit.addEventListener('submit', testPromise);
+
+function testPromise(e) {
+e.preventDefault();
+
+  let timeDelay = Number(formSubmit.delay.value);
+
+  for(i = 1; i <= formSubmit.amount.value; i += 1) {
+    createPromise(i, timeDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    timeDelay += Number(formSubmit.step.value);
   }
+}
+
+function createPromise(position, delay) {  // створення проміса
+  const obj = { position, delay };
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(obj);
+      } else {
+        // Reject
+        reject(obj);
+      }
+    }, delay);
+  });
 }
